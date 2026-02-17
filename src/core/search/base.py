79b9449e2150a57a22b756b121a59791e3822441
asuzukosi <keloasuzu@yahoo.com>
2026-02-17 21:04:29 +0000

    feat: add implementation of the keyword search

--- src/core/search/base.py
+++ src/core/search/base.py
@@ -1,8 +1,9 @@
 from src.core.data import QueryData, SearchResult, SearchConfig
 from abc import ABC, abstractmethod
+from src.data_loader.base import BaseDataLoader
 
 class BaseSearch(ABC):
-    def __init__(self, config: SearchConfig):
+    def __init__(self, config: SearchConfig, data_loader: BaseDataLoader):
         self._config = config
 
     @abstractmethod
--- src/core/search/keyword_search.py
+++ src/core/search/keyword_search.py
@@ -1,9 +1,19 @@
 from src.core.search.base import BaseSearch
 from src.core.data import QueryData, SearchResult, SearchConfig
+from src.data_loader.base import BaseDataLoader
 
 class KeywordSearch(BaseSearch):
-    def __init__(self, config: SearchConfig):
+    def __init__(self, config: SearchConfig, data_loader: BaseDataLoader):
         self._config = config
+        self._data_loader = data_loader
 
-    def search(self, query: QueryData) -> SearchResult:
-        pass
+    def search(self, query: QueryData, num_k:int=10) -> SearchResult:
+        data = self._data_loader.load()
+        results = []
+        for item in data:
+            if query.query.lower() == item.key.lower():
+                results.append(item)
+                # break when k items reached
+                if len(results) >= num_k:
+                    break
+        return SearchResult(query=query.query, context=query.context, results=results)
