e6f97159248f2ebfd07ad49aaf0dc6e1de26615e
asuzukosi <keloasuzu@yahoo.com>
2026-02-21 05:30:21 +0000

    feat: add new methods for combining weighted results in hybrid search class implementation

--- src/core/search/hybrid_search/hybrid_search.py
+++ src/core/search/hybrid_search/hybrid_search.py
@@ -18,7 +18,17 @@ class HybridSearch(BaseSearch):
     def search(self, query: QueryData, num_k:int=10) -> SearchResult:
         return self._bm_25_search(query, num_k)
     
+    def _semantic_search(self, query: QueryData, num_k:int=10) -> SearchResult:
+        return self._index.index_based_search(query, num_k)
+    
     def weighted_search(self, query: QueryData, num_k:int=10) -> SearchResult:
+        bm25_results = self._bm_25_search(query, num_k * 100)
+        semantic_results = self._semantic_search(query, num_k * 100)
+        weighted_results = self.hybrid_score(self.normalize_scores(bm25_results), self.normalize_scores(semantic_results))
+        return weighted_results
+    
+    def combine_results(self, bm25_results: List[SearchResult], semantic_results: List[SearchResult]) -> List[SearchResult]:
+        # TODO: combine the scores from both the bm25 scores and the semantic scores
         raise NotImplementedError
     
     def rrf_search(self, query: QueryData, num_k:int=10) -> SearchResult:
@@ -31,3 +41,7 @@ class HybridSearch(BaseSearch):
             return [1.0 for _ in scores]
         range_score = max_score - min_score
         return [(score - min_score) / range_score for score in scores]
+    
+
+    def hybrid_score(self, bm25_score: List[float], semantic_score: List[float], alpha: float=0.5) -> List[float]:
+        return [alpha * bm25_score[i] + (1 - alpha) * semantic_score[i] for i in range(len(bm25_score))]
